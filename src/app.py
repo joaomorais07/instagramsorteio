7e926c8a662bbae3925c25510c3c2c3b69b79549
edas6510 <joaomorais832@gmail.com>
2022-09-29 12:34:43 -0300

    versao 1.1

---
+++ src/app.py
@@ -0,0 +1,11 @@
+﻿from flask import Flask
+from src.routes.routes import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "senha"
+
+app.add_url_rule(routes["index_route"],view_func=routes["index_controller"])
+app.add_url_rule(routes["cadastro_route"],view_func=routes["cadastro_controller"])
+app.add_url_rule(routes["login_route"],view_func=routes["login_controller"])
+app.add_url_rule(routes["principal_route"],view_func=routes["principal_controller"])
+
---
+++ src/controllers/controller.py
@@ -0,0 +1,83 @@
+﻿from flask.views import MethodView
+from src.db import *
+from flask import render_template, request, redirect, url_for, session, flash
+import re
+
+class index (MethodView):
+    def get(self):
+        return render_template('index.html')
+
+class login (MethodView):
+    def get(self):
+        return render_template('login.html')
+
+    def post(self): 
+        if request.method == 'POST' and 'nome_usuario' in request.form and 'senha' in request.form: 
+            nome_usuario = request.form['nome_usuario'] 
+            senha = request.form['senha'] 
+            conexao = mysqlconector()
+            cursor = conexao.cursor()
+            cursor.execute('SELECT * FROM usuarios WHERE nome_usuario = %s AND senha = %s', (nome_usuario, senha)) 
+            account = cursor.fetchone()
+            if account: 
+                session['logado'] = True
+                #session['id'] = account['id'] 
+                #session['nome_usuario'] = account['nome_usuario'] 
+                #flash('Login efetuado com sucesso!')
+                return redirect('/principal') 
+            else: 
+                flash("Nome de usuario ou senha incorreto!")
+            return redirect('/login')
+  
+'''@app.route('/desconetar') 
+def desconectar(): 
+    session.pop('logado', None) 
+    session.pop('id', None) 
+    session.pop('nome_usuario', None) 
+    return redirect(url_for('login'))'''
+
+class cadastro (MethodView):
+    def get(self):
+        return render_template('cadastro.html')
+
+    def post(self): 
+        if request.method == 'POST' and 'nome_usuario' in request.form and 'email' in request.form and 'senha' in request.form : 
+            nome_usuario = request.form['nome_usuario'] 
+            email = request.form['email'] 
+            senha = request.form['senha'] 
+            conexao = mysqlconector()
+            cursor = conexao.cursor()
+            cursor.execute('SELECT * FROM usuarios WHERE nome_usuario = %s', (nome_usuario,)) 
+            usuarios = cursor.fetchone()
+            if usuarios: 
+                flash("Nome de usuario ja existente")
+                return redirect('/cadastro')
+            cursor.execute('SELECT * FROM usuarios WHERE email = %s', (email,)) 
+            usuarios = cursor.fetchone()
+            if usuarios:
+                flash("Email ja existente2")
+                return redirect('/cadastro')
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email): 
+                flash("Email invalido")
+                return redirect('/cadastro')
+            elif not re.match(r'[A-Za-z0-9]+', nome_usuario): 
+                flash("O nome de usuário deve conter apenas caracteres e números!")
+                return redirect('/cadastro')
+            elif not nome_usuario or not senha or not email: 
+                flash("Por favor, preencha o formulário !")
+                return redirect('/cadastro')
+            else: 
+                cursor.execute('INSERT INTO usuarios VALUES (%s, %s, %s, %s)', (0, nome_usuario, email, senha)) 
+                conexao.commit()
+                cursor.close()
+                conexao.close()
+                flash("Cadastro realizado com sucesso")
+        elif request.method == 'POST': 
+            flash('Por favor, preencha o formulário!')
+            return redirect('/cadastro')
+        return render_template('cadastro.html')
+
+
+class principal (MethodView):
+    def get(self):
+        return render_template('principal.html')
