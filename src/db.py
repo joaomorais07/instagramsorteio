1e68517d953d4917cac710fede31dbfa07d0b39f
joaomorais07 <89816148+joaomorais07@users.noreply.github.com>
2023-11-14 09:26:34 -0300

    Update db.py

--- src/db.py
+++ src/db.py
@@ -4,7 +4,7 @@ from mysql.connector import Error
 def mysqlconector():
     '''Takes no args, and returns a connection to MYDB via MYSQL.'''
     try:
-        conexao = mysql.connector.connect(user='ct54qfrgminot5og0met', password='pscale_pw_CcdN1ueBYks33JJuHb9CLTQFouh3B7wfewEIG7O72Qy', host='aws.connect.psdb.cloud', database='instagram_sorteio', port=3306)
+        conexao = mysql.connector.connect(user='8ijaiyutxz7hhgicc7iy', password='pscale_pw_M43YdDeRLGUijbfSKWhPrGhUHySVmRhgN0Jx46FWwxZ', host='aws.connect.psdb.cloud', database='instagram_sorteio', port=3306)
     except mysql.connector.Error as err:
         if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
